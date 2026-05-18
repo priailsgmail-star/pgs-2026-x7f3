adde75d43caf4bb881587475af8ea73f8cb1f275
priailsgmail-star <priails.gmail@gmail.com>
2026-05-18 13:27:51 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker — Programme Santé Lorenzo
 // Cache les fichiers essentiels pour usage hors ligne
 
-const CACHE_NAME = 'sante-ls-v1-2-sem2';
+const CACHE_NAME = 'sante-ls-v1-3-eval-contextuel';
 const ASSETS = [
   './',
   './index.html',
